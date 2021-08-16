1e491d311011a9db7b982728aff5fa775eb61094
Siddharth Singh Rana <siddharth_sr@cs.iitr.ac.in>
2021-08-16 17:08:54 +0530

    user allowed datetime-local input

--- lecture/polls/forms.py
+++ lecture/polls/forms.py
@@ -2,8 +2,11 @@ from django import forms
 from django.forms import ModelForm
 from .models import TodoItem, TodoList
 
+class DateInput(forms.DateInput):
+    input_type = 'datetime-local'
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = TodoItem
+        widgets = {'due_date': DateInput()}
         exclude = ['todo_list']
